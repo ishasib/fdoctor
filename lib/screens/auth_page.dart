c49e35738b92f6faa8fe7748c5275336d483f893
ishasib <arifulhasib007@gmail.com>
2023-08-09 22:02:32 +0600

    Ha

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -17,91 +17,91 @@ class _AuthPageState extends State<AuthPage> {
     Config().init(context);
     //build login text field
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 15,
-      ),
-      child: SafeArea(
+        body: SafeArea(
+      child: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            AppText.enText['welcome_text']!,
-            style: const TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Config.spaceSmall,
-          Text(
-            AppText.enText['singIn_text']!,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Config.spaceSmall,
-          const LoginForm(), //let's build text form field for login
-          Config.spaceSmall,
-          Center(
-            child: TextButton(
-              onPressed: () {},
-              child: Text(
-                AppText.enText['forgot_password']!,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                AppText.enText['welcome_text']!,
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Config.spaceSmall,
+              Text(
+                AppText.enText['singIn_text']!,
                 style: const TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black,
                 ),
               ),
-            ),
-          ),
-          //add social button sign in
-          const Spacer(),
-          Center(
-            child: Text(
-              AppText.enText['social_login']!,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-                color: Colors.grey.shade500,
+              Config.spaceSmall,
+              const LoginForm(), //let's build text form field for login
+              Config.spaceSmall,
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    AppText.enText['forgot_password']!,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Config.spaceSmall,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const <Widget>[
-              SocialButton(social: 'google'),
-              SocialButton(social: 'facebook'),
+              //add social button sign in
+              const Spacer(),
+              Center(
+                child: Text(
+                  AppText.enText['social_login']!,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey.shade500,
+                  ),
+                ),
+              ),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const <Widget>[
+                  SocialButton(social: 'google'),
+                  SocialButton(social: 'facebook'),
+                ],
+              ),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    AppText.enText['signup_text']!,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
-          Config.spaceSmall,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                AppText.enText['signup_text']!,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.grey.shade500,
-                ),
-              ),
-              const Text(
-                'Sign Up',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          )
-        ],
-      )),
+        ),
+      ),
     ));
   }
 }
